bb6c238ad1833b244142e67485781df07b221ed0
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-09 12:45:31 -0800

    Now, I have learnt my lessons the hard way! 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
-#include <stdio.h>                                                                   
-/**                                                                                                                      
- * main - Entry                                                                                                                         
- * Return: Always 0 (Success)                                                                                                                    
- **/ 
+#include <studio.>
+/**
+ * main - Entry
+ *
+ * Description - 'using printf function'
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n" \n)
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n" \n)
+		return (0);
 }
