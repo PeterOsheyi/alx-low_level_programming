d1314865d75dc50a6bc1220aecc3ab129e864bfe
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-09 20:36:50 -0800

    5an6 again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  * Return: Always 0 (Success)
  *
  **/
-int main()
+int main(void)
 {
 	int intType;
 	float floatType;
 	double doubleType;
 	char charType;
 	/*size of types*/
-	printf(sizeof(intType));
-	printf(sizeof(floatType));
-	printf(sizeof(doubleType));
-	printf(sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes \n", sizeof(charType));
 		return (0);
 }
