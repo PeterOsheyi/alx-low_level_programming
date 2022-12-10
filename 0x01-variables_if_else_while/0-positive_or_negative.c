74cc3e434f91527d5748e85a93ca1a2aa61273fd
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-10 05:36:04 -0800

    last chance

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 	int n;
 
 	scanf("%d", &n);
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
 	}
 	else
 		printf("%d is negative\n", n);
