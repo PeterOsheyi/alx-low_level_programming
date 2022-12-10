9f29eab85b59a3e6a57a8604f97339cb6b9ef0fd
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-09 20:19:20 -0800

    6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "size of various types"
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	printf(sizeof(intType));
+	printf(sizeof(floatType));
+	printf(sizeof(doubleType));
+	printf(sizeof(charType));
+		return (0);
+}
