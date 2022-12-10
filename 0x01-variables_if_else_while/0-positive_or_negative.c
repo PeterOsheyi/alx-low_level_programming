b71ea1d8325b813a198b57a6befb8a47bd8acc66
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-10 05:11:00 -0800

    im fkng tired of errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,18 @@
  * Return: Always 0 (Success)
  *
  **/
-int n;
 int main(void)
 {
+	int n;
+
 	if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else
 		printf("%d is negative\n", n);
 	return (0);
