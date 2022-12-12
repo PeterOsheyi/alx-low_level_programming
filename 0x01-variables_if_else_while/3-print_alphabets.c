e684de32004a2514f74b6ba757788f0a1cf1064c
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-12 09:54:49 -0800

    I am no longer going to be lazy with my work

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "print in lowercase"
+ *
+ *
+ * Return: Always 0 (Succes)
+ *
+ **/
+int main(void)
+{
+	char lowerC = 'a';
+	char upperC = 'A';
+
+	for (lowerC = 'a'; lowerC <= 'z'; lowerC++)
+		putchar(lowerC);
+	for (upperC = 'A'; upperC <= 'Z'; upperC++)
+		putchar(upperC);
+	return (0);
+}
