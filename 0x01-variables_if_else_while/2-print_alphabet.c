55ebef9fd0bdda17b326b36a97e5d43c7f51e227
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-12 09:19:33 -0800

    Oh I did it once this time

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "print in lowercase"
+ *
+ * Return: Always 0 (Succes)
+ *
+ **/
+int main(void)
+{
+	char lowerC = 'a';
+
+	for (lowerC = 'a'; lowerC <= 'z'; lowerC++)
+		putchar(lowerC);
+	return (0);
+}
