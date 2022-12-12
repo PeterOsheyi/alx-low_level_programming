d33d5e8653264f6109d5cc97a275d5fd515467d6
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-12 15:49:13 -0800

    I did this 8 once also

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,10 +12,10 @@ int main(void)
 	char hex;
 	int num;
 
+	for (num = '0'; num >= '9'; num++)
+		putchar(num);
 	for (hex = 'a'; hex <= 'f'; hex++)
 		putchar(hex);
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
 	putchar('\n');
 	return (0);
 }
