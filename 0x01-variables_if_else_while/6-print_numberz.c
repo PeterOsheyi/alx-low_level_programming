42058eab6ae347c5a75687023821aa7959523fd1
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-12 15:03:14 -0800

    I did this 6 less than 5 times

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "6th question"
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
