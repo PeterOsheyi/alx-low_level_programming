78caa59fb1368a2223da1c53e1fa543d819c972c
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-12 16:24:37 -0800

    just add spaces to size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	float floatType;
 	char charType;
 	/*size of types of data in C*/
-	printf("Size of char: %zu byte(s) \n", sizeof(charType));
-	printf("Size of int: %zu byte(s) \n", sizeof(intType));
-	printf("Size of long int: %zu byte(s) \n", sizeof(longIntType));
-	printf("Size of long long int: %zu byte(s) \n", sizeof(longLongInt));
-	printf("Size of float: %zu byte(s) \n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s) \n", sizeof(charType));
+	printf("Size of an int: %zu byte(s) \n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s) \n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s) \n", sizeof(longLongInt));
+	printf("Size of a float: %zu byte(s) \n", sizeof(floatType));
 		return (0);
 }
