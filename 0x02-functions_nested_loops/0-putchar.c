9ed976570174deb210a9224ac0ed01873f8f16a8
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-13 21:58:31 -0800

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "nested loops program"
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	char p = _putchar;
+
+	printf("%s\n", p);
+}
