ad69cb2de77514b19433756bacf535d23822ef37
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-19 10:32:22 -0800

    reset to 98

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "program of numbers with commas"
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	int n, i;
+	scanf("%d";
