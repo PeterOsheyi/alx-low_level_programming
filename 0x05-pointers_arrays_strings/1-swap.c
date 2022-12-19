a0ead2c084749e73b54cb231d133bf40231905a7
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-19 10:47:48 -0800

    error correction a and b

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int-Swaps the values of 2 integers.
- * a is First argument to be swapped.
- * b is second argument to be swapped.
+ * @a is First argument to be swapped.
+ * @b is second argument to be swapped.
  * Return:
  **/
 
