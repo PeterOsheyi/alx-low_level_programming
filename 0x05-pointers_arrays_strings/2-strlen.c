c0414c42dc720ceabae7c7d6b9e98fce04bade5b
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-19 17:37:11 -0800

    2 whitesoace error

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *_strlen-Finds the length of a string.
- *@s:String pointer to the string whose length is to be found.
- *Return: returns the length of the string.
+ *@s:String pointer to the string whose length is to be found
+ *Return: returns the length of the string
  **/
-
 int _strlen(char *s)
 {
-	int p = 0;	
+	int p = 0;
+
 	while (*(s + p) != 0)
 	{
 		p++;
