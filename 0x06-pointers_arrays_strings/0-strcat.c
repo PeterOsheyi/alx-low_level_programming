4829d8d20caf90c43df5108597579a61506b5514
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-23 18:44:28 -0800

    betty error on 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,19 @@
  **/
 char *_strcat(char *dest, char *src)
 {
-	    char *s = dest;
+	char *s = dest;
 
-	    while (*dest != '\0')
-	    {
-		    dest++;
-	    }
-	    while (*src != '\0')
-	    {
-		    *dest = *src;
-		    dest++;
-		    src++;
-	    }
-	    *dest = '\0';
-	    return (s);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
