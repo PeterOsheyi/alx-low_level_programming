126c6ca8d99f3f46704f876d45fb8cb7870dce9e
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2022-12-23 19:55:16 -0800

    3-strcmp.c correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,19 @@
 int _strcmp(char *st1, char *st2)
 {
 	while (((*st1 != '\0') && (*st2 != '\0')) && (*st1 == *st2))
+	{
 		st1++;
-	st2++;
-}
+		st2++;
+	}
 
-if (*st1 == *st2)
-{
+	if (*st1 == *st2)
+	{
 	return (0);
-}
-else
-{
+	}
+
+	else
+	{
 	return (*st1 - *st2);
-}
+	}
 }
 
