4871acdaeaa823d5f11bdd5cf401d5ef28aeaa25
PeterOsheyi <peteroluwashuwasheyioluokun@gmail.com>
2023-01-10 21:50:31 -0800

    0-create_array.c again

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,18 +16,6 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	/* if (size == !0)
-	{
-		a = (char *)malloc(size * sizeof(c));
-		if (array != NULL)
-		{
-			for (i = 0; i < size; i++)
-				array[i] = c;
-		}
-	}
-	return (array);
-} 
-*/
 a = (char *) malloc(size * sizeof(c));
 if (a == 0)
 {
